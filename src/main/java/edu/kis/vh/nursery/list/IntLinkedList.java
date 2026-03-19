8177c01657457cc1b60ba314ba2027538590fc04
WiktorRzepka <265846438+WiktorRzepka@users.noreply.github.com>
2026-03-19 16:59:48 +0100

    9. ustawienie mozliwie najmniejszej widocznosci dla atrybutow klas

--- src/main/java/edu/kis/vh/nursery/list/IntLinkedList.java
+++ src/main/java/edu/kis/vh/nursery/list/IntLinkedList.java
@@ -2,8 +2,8 @@ package edu.kis.vh.nursery.list;
 
 public class IntLinkedList {
 
-    Node last;
-    int size;
+    protected Node last;
+    private int size;
 
     public void push(int i) {
         if (last == null)
