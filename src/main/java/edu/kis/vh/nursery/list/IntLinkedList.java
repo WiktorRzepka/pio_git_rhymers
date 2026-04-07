26545f4ad075d478903d37b0db2a2c1a50c1cafc
WiktorRzepka <265846438+WiktorRzepka@users.noreply.github.com>
2026-04-07 12:07:36 +0200

    17. stworzenie dokumentacji dla klasy IntLinkedList

--- src/main/java/edu/kis/vh/nursery/list/IntLinkedList.java
+++ src/main/java/edu/kis/vh/nursery/list/IntLinkedList.java
@@ -1,11 +1,19 @@
 package edu.kis.vh.nursery.list;
 
+/**
+ * Klasa reprezentująca strukturę stosu dla liczb całkowitych.
+ * Opiera się na liście dwukierunkowej Node.
+ */
 public class IntLinkedList {
 
     private static final int STACK_EMPTY=-1;
     private Node last;
     private int size;
 
+    /**
+     * Dodaje nowy element na szczyt stosu.
+     * @param i wartość całkowita do dodania
+     */
     public void push(int i) {
         if (last == null)
             last = new Node(i);
@@ -16,20 +24,36 @@ public class IntLinkedList {
         }
     }
 
+    /**
+     * Sprawdza czy struktura jest pusta.
+     * @return true, jeśli struktura jest pusta, w przeciwnym wypadku false.
+     */
     public boolean isEmpty() {
         return last == null;
     }
 
+    /**
+     * Sprawdza czy struktura jest pełna.
+     * @return zawsze false - struktura jest dynamiczna
+     */
     public boolean isFull() {
         return false;
     }
 
+    /**
+     * Zwraca wartość ostatnio dodanego elementu, bez usuwania go ze stosu.
+     * @return wartość ostatnio dodanego elementu lub wartość STACK_EMPTY, jeśli struktura jest pusta.
+     */
     public int top() {
         if (isEmpty())
             return STACK_EMPTY;
         return last.getValue();
     }
 
+    /**
+     * Usuwa ostatnio dodany element i go zwraca.
+     * @return wartość ostatnio dodanego elementu lub wartość STACK_EMPTY, jeśli struktura jest pusta.
+     */
     public int pop() {
         if (isEmpty())
             return STACK_EMPTY;
@@ -38,6 +62,10 @@ public class IntLinkedList {
         return ret;
     }
 
+    /**
+     * Zwraca referencję do ostatnio dodanego węzła do stosu.
+     * @return obiekt klasy Node - ostatnio dodany węzeł lub null.
+     */
     Node getLast() {
         return last;
     }
